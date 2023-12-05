17b72b8d129f1e718e687060f558ffbe80fda732
Jose <jose@numa.com>
2023-12-05 15:33:57 -0700

    js intro

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log("hello world");
+console.log("otro hellow world");
+
+// este es un comentario ignorado
+// console.log("este no se va a imprimir")
+
+/* 
+    este es un comentario de varias lineas
+    console.log("este no se va a imprimir")
+    console.log("este no se va a imprimir")
+*/
+
+// Javascript: 
+ // 1- es un lenguaje de programacion que se ejecuta en el navegador
+ // 2- es un lenguaje de programacion que se ejecuta en el servidor
+ // 3- usado para crear aplicaciones moviles, de escritorio, y web
+
+// Caracteristicas de Javascript
+// 1- es un lenguaje de programacion interpretado
+// 2- es un lenguaje de programacion debilmente tipado
+// 3 - es un lenguaje de programacion con tipos de datos dinamicos
+
+var nombre;
+var age = 25;
+
+console.log(nombre, age);
+
+nombre = "Jose";
+
+
+console.log(nombre);
+
+// 4- es asincrono non-blocking
+// 5- es un lenguaje de programacion orientado a prototipos
+
