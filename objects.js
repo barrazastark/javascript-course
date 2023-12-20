41eaa953b05a489085b8c15d6af16b5f73440e77
Jose <jose@numa.com>
2023-12-20 16:33:20 -0700

    intro to objects

---
+++ objects.js
@@ -0,0 +1,43 @@
+
+// Object is javascript: is non primitive data type
+
+// JSON: Javascript Object Notation
+
+// is collection of key value pairs
+
+// Objects in programming are like objects in real life
+
+var person = {
+    name: "John",
+    age: 32,
+    isMarried: false,
+    address: {
+        street: "123 Main St",
+        city: "New York",
+        state: "NY"
+    }
+}
+
+// console.log(typeof person)
+
+// var key = prompt("What property do you want to access?");
+
+// how to access object properties
+
+// dot notation
+// console.log(person.name);
+
+// bracket notation
+// console.log(person[key]);
+
+// console.log(person.address.city);
+
+// built in methods
+
+/* console.log(Object.keys(person));
+
+console.log(Object.values(person));
+
+console.log(Object.entries(person));
+
+*/
